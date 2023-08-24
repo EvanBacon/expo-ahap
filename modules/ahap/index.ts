2d07335eb8b4d68dcb236a2d3953a75eb3363ed7
evanbacon <bacon@expo.io>
2023-08-24 18:57:35 -0500

    wip

--- modules/ahap/index.ts
+++ modules/ahap/index.ts
@@ -7,14 +7,183 @@ import {
 // Import the native module. On web, it will be resolved to Ahap.web.ts
 // and on native platforms to Ahap.ts
 import AhapModule from "./src/AhapModule";
-import AhapView from "./src/AhapView";
 import { ChangeEventPayload, AhapViewProps } from "./src/Ahap.types";
 
 // Get the native constant value.
 export const PI = AhapModule.PI;
 
+/**
+ * Parameters used to modify individual haptic and/or audio events.
+ *
+ * Event parameters are specified as part of the creation of a CHHapticEvent or in an event definition in a haptic pattern.
+ * The combination of Event parameters will determine the character of the haptic or audio event.
+ */
+type CHHapticEventParameterID =
+  /**
+   * The perceived intensity (volume) of a haptic event.
+   * Range: 0.0 (maximum attenuation) to 1.0 (no attenuation).
+   */
+  | "HapticIntensity"
+
+  /**
+   * Depending on the event's signal content, this may map to frequency, frequency content (i.e., filtering),
+   * or some other signal processing.
+   * Range: 0.0 (least sharp) to 1.0 (most sharp).
+   */
+  | "HapticSharpness"
+
+  /**
+   * The attack time adjuster for a Continuous event's envelope.
+   * Range: 0.0 to 1.0, with default: 0.0 (shortest attack time).  Higher values increase the time exponentially.
+   * Not all event types respond to this parameter.
+   */
+  | "AttackTime"
+
+  /**
+   * The decay time adjuster for a Continuous event's envelope.
+   * Range: 0.0 to 1.0, with default: 0.0 (shortest decay time).  Higher values increase the time exponentially.
+   * For envelope decay to take effect, the `Sustained` parameter must be set to 0.0.
+   * Not all event types respond to this parameter.*/
+  | "DecayTime"
+
+  /**
+   * The release time adjuster for a Continuous event's envelope.
+   * Range: 0.0 to 1.0, with default: 0.0 (shortest release time).  Higher values increase the time exponentially.
+   * Not all Continuous event types respond to this parameter.
+   */
+  | "ReleaseTime"
+
+  /**
+   * A boolean (1.0 or 0.0) which indicates whether a Continuous event sustains for its specified duration
+   * (using an Attack/Release envelope) or whether the event ends when its envelope decay segment reaches its minimum
+   * (i.e., using an Attack/Decay envelope with no sustain).
+   * Default: 1.0 (sustained, Attack/Release).
+   */
+  | "Sustained"
+
+  /** The volume of an audio event.
+   * Range: 0.0 (maximum attenuation) to 1.0 (no attenuation).
+   */
+  | "AudioVolume"
+
+  /**
+   * The pitch adjuster for audio events.
+   * Range: -1.0 (lowest pitch) to 1.0 (highest pitch).
+   */
+  | "AudioPitch"
+
+  /**
+   * The stereo panning for an audio event.
+   * Range: -1.0 (panned full left) to 1.0 (panned full right).
+   * Default: 0.0 (panned center).
+   */
+  | "AudioPan"
+  /**
+   * The high frequency content an audio event.
+   * Range: 0.0 (frequency content reduced the most) to 1.0 (no reduction of frequency content).
+   * Default: 1.0.
+   */
+  | "AudioBrightness";
+
+/**
+ * Parameters used to dynamically modify all haptic or audio events within a pattern.
+ *
+ * Dynamic parameters are not tied to specific events; each dynamic parameter modifies (modulates) the
+ * effect of the corresponding event parameter for events which respond to the parameter.
+ *
+ * The `CHHapticDynamicParameterIDHaptic` types only affect haptic event types, and the `CHHapticDynamicParameterIDAudio`
+ * types only affect audio event types.  Not all `CHHapticDynamicParameterID`s will have an effect on every `CHHapticEventType`.
+ */
+type CHHapticDynamicParameterID =
+  /**
+   * Adjusts the intensity of all active and future haptic events.
+   * @range 0.0 (event intensities reduced by the maximum amount) to 1.0 (no effect on event intensities).
+   * @default 1.0
+   */
+  | "HapticIntensityControl"
+
+  /**
+   * This will adjust the frequency, frequency content (i.e., filtering), or other aspects of all active and future haptic events.
+   * @range -1.0 (less sharp) to 1.0 (more sharp).
+   * @default 0.0 (no effect).
+   */
+  | "HapticSharpnessControl"
+  /**
+   * Adjusts the envelope attack time of all active and future haptic events.
+   * @range -1.0 (event attacks shorter) to 1.0 (event attacks longer).
+   * @default 0.0 (no effect).
+   * Not all haptic event types respond to this parameter.
+   */
+  | "HapticAttackTimeControl"
+
+  /**
+   * Adjusts the envelope decay time of all active and future Transient haptic events.
+   * @range -1.0 (event decays shorter) to 1.0 (event decays longer).
+   * @default 0.0 (no effect).
+   * Not all haptic event types respond to this parameter.
+   */
+  | "HapticDecayTimeControl"
+
+  /**
+   * Adjusts the envelope release time of all active and future Continuous haptic events.
+   * @range -1.0 (event releases shorter) to 1.0 (event releases longer).
+   * @default 0.0 (no effect).
+   * Not all haptic event types respond to this parameter.
+   */
+  | "HapticReleaseTimeControl"
+
+  /**
+   * Adjusts the volume of all active and future audio events.
+   * @range 0.0 (event intensities reduced by the maximum amount) to 1.0 (no effect).
+   * @default 1.0
+   */
+  | "AudioVolumeControl"
+
+  /**
+   * Adjusts the panning of all active and future audio events.
+   * @range -1.0 (events panned more left) to 1.0 (event panned more right).
+   * @default 0.0 (no effect).
+   */
+  | "AudioPanControl"
+  /**
+   * Adjusts the high frequency content of all active and future audio events.
+   * @range -1.0 (more filtering) to 1.0 (less filtering).
+   * @default 0.0 (no effect).
+   * */
+  | "AudioBrightnessControl"
+  /**
+   * Adjusts the transposition of the audio event.
+   * @range -1.0 to 1.0.  Negative values decrease pitch; positive values increase pitch.
+   * @default 0.0 (no effect).
+   */
+  | "AudioPitchControl"
+
+  /**
+   * Adjusts the envelope attack time of all active and future audio events.
+   * @range -1.0 (event attacks shorter) to 1.0 (event attacks longer).
+   * @default 0.0 (no effect).
+   * Not all audio event types respond to this parameter.
+   */
+  | "AudioAttackTimeControl"
+
+  /**
+   * Adjusts the envelope decay time of all active and future Transient audio events.
+   * @range -1.0 (event decays shorter) to 1.0 (event decays longer).
+   * @default 0.0 (no effect).
+   * Not all audio event types respond to this parameter.
+   */
+  | "AudioDecayTimeControl"
+
+  /**
+   * Adjusts the envelope release time of all active and future Continuous audio events.
+   * @range -1.0 (event releases shorter) to 1.0 (event releases longer).
+   * @default 0.0 (no effect).
+   * Not all audio event types respond to this parameter.
+   */
+  | "AudioReleaseTimeControl";
+
 type AhapEventPatternValue = {
-  ParameterID: "HapticIntensity" | "HapticSharpness" | "AudioVolume";
+  ParameterID: CHHapticEventParameterID;
   ParameterValue: number;
 };
 
@@ -46,7 +215,7 @@ type AhapEventPattern = {
 
 type AhapParameterCurvePattern = {
   ParameterCurve: {
-    ParameterID: "HapticIntensityControl";
+    ParameterID: CHHapticDynamicParameterID;
     Time: number;
     ParameterCurveControlPoints: AhapParameterCurveControlPoints[];
   };
@@ -61,6 +230,72 @@ type AhapType = {
   };
   Pattern: (AhapEventPattern | AhapParameterCurvePattern)[];
 };
+
+export class Player {
+  private id = Math.random().toString();
+
+  constructor(pattern: AhapType) {
+    AhapModule.register(this.id, pattern);
+  }
+
+  get isMuted(): boolean {
+    return AhapModule.getIsMuted(this.id);
+  }
+  set isMuted(value: boolean) {
+    AhapModule.setIsMuted(this.id, value);
+  }
+
+  get loopEnabled(): boolean {
+    return AhapModule.isLoopEnabled(this.id);
+  }
+  set loopEnabled(value: boolean) {
+    AhapModule.setLoopEnabled(this.id, value);
+  }
+
+  get playbackRate(): number {
+    return AhapModule.getPlaybackRate(this.id);
+  }
+  set playbackRate(value: number) {
+    AhapModule.setPlaybackRate(this.id, value);
+  }
+
+  get getLoopEnd(): number {
+    return AhapModule.getLoopEnd(this.id);
+  }
+  set setLoopEnd(value: number) {
+    AhapModule.setLoopEnd(this.id, value);
+  }
+
+  start(time?: number) {
+    AhapModule.action(this.id, "start", time);
+  }
+  stop(time?: number) {
+    AhapModule.action(this.id, "stop", time);
+  }
+  pause(time?: number) {
+    AhapModule.action(this.id, "pause", time);
+  }
+  resume(time?: number) {
+    AhapModule.action(this.id, "resume", time);
+  }
+  seek(time: number) {
+    AhapModule.action(this.id, "seek", time);
+  }
+  unregister() {
+    AhapModule.unregister(this.id);
+  }
+
+  sendParameters(params: unknown[], time?: number) {
+    AhapModule.send(this.id, params, time);
+  }
+
+  scheduleParameterCurve(
+    curve: AhapParameterCurvePattern["ParameterCurve"],
+    time?: number
+  ) {
+    AhapModule.schedule(this.id, curve, time);
+  }
+}
 
 export function playAhap(ahap: AhapType) {
   AhapModule.playAhap(ahap);
@@ -129,4 +364,4 @@ export function addChangeListener(
   return emitter.addListener<ChangeEventPayload>("onChange", listener);
 }
 
-export { AhapView, AhapViewProps, ChangeEventPayload };
+export { AhapViewProps, ChangeEventPayload };
--- modules/ahap/ios/AhapModule.swift
+++ modules/ahap/ios/AhapModule.swift
@@ -1,28 +1,184 @@
 import ExpoModulesCore
 import CoreHaptics
 
+struct CurvePoint: Record {
+    @Field
+    var Time: Float = 0
+    
+    @Field
+    var ParameterValue: Float = 0
+}
+
+struct Curve: Record {
+    @Field
+    var ParameterID: String
+    
+    @Field
+    var Time: Float = 0
+    
+    @Field
+    var ParameterCurveControlPoints: [CurvePoint]
+}
+
+
 public class AhapModule: Module {
+    var engine: CHHapticEngine?
+    var players: [String: CHHapticAdvancedPatternPlayer] = [:]
+    
+    func startEngine() {
+        do {
+            if engine == nil {
+                engine = try CHHapticEngine(audioSession: .sharedInstance())
+            }
+        } catch let error {
+            print("Error creating haptic pattern: \(error)")
+        }
+    }
+    
     // Each module class must implement the definition function. The definition consists of components
     // that describes the module's functionality and behavior.
     // See https://docs.expo.dev/modules/module-api for more details about available components.
     public func definition() -> ModuleDefinition {
-        var engine: CHHapticEngine?
+        
         
         // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
         // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
         // The module will be accessible from `requireNativeModule('Ahap')` in JavaScript.
         Name("Ahap")
         
+        // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+        Function("register") { (name: String, patternDict: [CHHapticPattern.Key: Any]) in
+            startEngine()
+            
+            if let engine = engine {
+                // Create a pattern from the dictionary.
+                let pattern = try CHHapticPattern(dictionary: patternDict)
+                let player = try? engine.makeAdvancedPlayer(with: pattern)
+                players[name] = player
+            }
+        }
+        
+        // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+        Function("unregister") { (name: String) in
+            players[name] = nil
+        }
+        
+        // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+        Function("action") { (name: String, action: String, time: TimeInterval?) in
+            if let engine = engine {
+                
+                if let player = players[name] {
+                    do {
+                        if (action == "start") {
+                            // Start the haptic engine to prepare it for use.
+                            do {
+                                try engine.start()
+                            } catch let error {
+                                print("The engine failed to start with error: \(error)")
+                            }
+                            
+                            try player.start(atTime: time ?? CHHapticTimeImmediate)
+                        } else if (action == "stop") {
+                            try player.stop(atTime: time ?? CHHapticTimeImmediate)
+                        } else if (action == "pause") {
+                            try player.pause(atTime: time ?? CHHapticTimeImmediate)
+                        } else if (action == "resume") {
+                            try player.resume(atTime: time ?? CHHapticTimeImmediate)
+                        } else if (action == "seek") {
+                            try player.seek(toOffset: time ?? CHHapticTimeImmediate)
+                        }
+                    } catch let error {
+                        print("Error starting haptic player: \(error)")
+                    }
+                }
+            }
+        }
+        
+        // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+        Function("send") { (name: String, params: [CHHapticDynamicParameter], time: TimeInterval?) in
+            if let player = players[name] {
+                do {
+                    try player.sendParameters(params, atTime: time ?? CHHapticTimeImmediate)
+                } catch let error {
+                    print("Error starting haptic player: \(error)")
+                }
+            }
+        }
+        
+        // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+        Function("schedule") { (name: String, curve: Curve, time: TimeInterval?) in
+            
+            if let player = players[name] {
+                // Convert curve to CHHapticParameterCurve
+                let curve = CHHapticParameterCurve(parameterID: CHHapticDynamicParameter.ID(rawValue: curve.ParameterID), controlPoints: curve.ParameterCurveControlPoints.map(
+                    { (point: CurvePoint) -> CHHapticParameterCurve.ControlPoint in
+                        return CHHapticParameterCurve.ControlPoint(relativeTime: TimeInterval(point.Time), value: point.ParameterValue)
+                    }
+                ), relativeTime: TimeInterval(curve.Time))
+                
+                do {
+                    try player.scheduleParameterCurve(curve, atTime: time ?? CHHapticTimeImmediate)
+                } catch let error {
+                    print("Error starting haptic player: \(error)")
+                }
+            }
+        }
+        
+        Function("getLoopEnabled") { (name: String) -> Bool? in
+            if let player = players[name] {
+                return player.loopEnabled
+            }
+            return nil
+        }
+        
+        Function("setLoopEnabled") { (name: String, loopEnabled: Bool) in
+            if let player = players[name] {
+                player.loopEnabled = loopEnabled
+            }
+        }
+        
+        Function("getIsMuted") { (name: String) -> Bool? in
+            if let player = players[name] {
+                return player.isMuted
+            }
+            return nil
+        }
+        
+        Function("setIsMuted") { (name: String, isMuted: Bool) in
+            if let player = players[name] {
+                player.isMuted = isMuted
+            }
+        }
+        
+        Function("getPlaybackRate") { (name: String) -> Float? in
+            if let player = players[name] {
+                return player.playbackRate
+            }
+            return nil
+        }
+        
+        Function("setPlaybackRate") { (name: String, playbackRate: Float) in
+            if let player = players[name] {
+                player.playbackRate = playbackRate
+            }
+        }
+        
+        Function("getLoopEnd") { (name: String) -> Double? in
+            if let player = players[name] {
+                return player.loopEnd
+            }
+            return nil
+        }
+        
+        Function("setLoopEnd") { (name: String, loopEnd: Double) in
+            if let player = players[name] {
+                player.loopEnd = TimeInterval(loopEnd)
+            }
+        }
         
         // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
         Function("playAhap") { (patternDict: [CHHapticPattern.Key: Any]) in
-            do {
-                if engine == nil {
-                    engine = try CHHapticEngine(audioSession: .sharedInstance())
-                }
-            } catch let error {
-                print("Error creating haptic pattern: \(error)")
-            }
+            startEngine()
             
             if let engine = engine {
                 // Start the haptic engine to prepare it for use.
@@ -34,15 +190,15 @@ public class AhapModule: Module {
                 
                 // Create a pattern from the dictionary.
                 let pattern = try CHHapticPattern(dictionary: patternDict)
-                let collisionPlayerSmall = try? engine.makePlayer(with: pattern)
-
+                let player = try? engine.makeAdvancedPlayer(with: pattern)
+                
                 do {
-                    try collisionPlayerSmall?.start(atTime: CHHapticTimeImmediate)
+                    try player?.start(atTime: CHHapticTimeImmediate)
                 } catch let error {
                     print("Error starting haptic player: \(error)")
                 }
             }
         }
-
+        
     }
 }
