3971e290c45894fbf891dbb2c374bb2373871769
evanbacon <bacon@expo.io>
2023-08-24 17:41:42 -0500

    working

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Ahap from "local:ahap";
+import React from "react";
 
 export default function App() {
+  const play = React.useCallback(() => {
+    const json = {};
+    Ahap.hello();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={() => play()}>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- modules/ahap/ios/AhapModule.swift
+++ modules/ahap/ios/AhapModule.swift
@@ -1,44 +1,48 @@
 import ExpoModulesCore
+import CoreHaptics
 
 public class AhapModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('Ahap')` in JavaScript.
-    Name("Ahap")
+    // Each module class must implement the definition function. The definition consists of components
+    // that describes the module's functionality and behavior.
+    // See https://docs.expo.dev/modules/module-api for more details about available components.
+    public func definition() -> ModuleDefinition {
+        var engine: CHHapticEngine?
+        
+        // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
+        // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
+        // The module will be accessible from `requireNativeModule('Ahap')` in JavaScript.
+        Name("Ahap")
+        
+        
+        // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+        Function("hello") { (patternDict: [CHHapticPattern.Key: Any]) in
+            do {
+                if engine == nil {
+                    engine = try CHHapticEngine(audioSession: .sharedInstance())
+                }
+            } catch let error {
+                print("Error creating haptic pattern: \(error)")
+            }
+            
+            if let engine = engine {
+                // Start the haptic engine to prepare it for use.
+                do {
+                    try engine.start()
+                } catch let error {
+                    print("The engine failed to start with error: \(error)")
+                }
+                
+                // Create a pattern from the dictionary.
+                let pattern = try CHHapticPattern(dictionary: patternDict)
+                let collisionPlayerSmall = try? engine.makePlayer(with: pattern)
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
+                do {
+                    try collisionPlayerSmall?.start(atTime: CHHapticTimeImmediate)
+                } catch let error {
+                    print("Error starting haptic player: \(error)")
+                }
+            }
+        }
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
     }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(AhapView.self) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { (view: AhapView, prop: String) in
-        print(prop)
-      }
-    }
-  }
 }
